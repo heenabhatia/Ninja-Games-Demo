6b3d58858897630fa3e4a2e1b096b7c4f3441ebb
heenabhatia <heenabhatia363@gmail.com>
2021-04-30 00:36:41 +0300

    Committing collision changes

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,10 +6,11 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 // import { Application } from 'pixi.js'
 import { Sprite, Graphics, Texture, Container } from 'pixi.js';
 import * as PIXI from 'pixi.js';
-import { AnimationMixer, Material, PixelFormat, Object3D } from 'three';
+import { AnimationMixer, Box3, Object3D } from 'three';
 
-let mesh: THREE.Object3D;
-let cube: THREE.Object3D;
+let mesh: Object3D;
+let cube: Object3D;
+let cubes: Object3D[];
 let animations, mixer: AnimationMixer;
 let attackAction: THREE.AnimationAction;
 let bragAction: THREE.AnimationAction;
@@ -53,7 +54,7 @@ const ambient = new THREE.AmbientLight( 0x0000ff, 1 );
 scene_3D.add( ambient );
 
 const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 20, 0 );
+hemiLight.position.set( 20, 20, 0 );
 scene_3D.add( hemiLight );
 
 const texture = new THREE.TextureLoader().load( 
@@ -81,6 +82,7 @@ loader.load( 'textures/cibus_ninja.glb', function ( gltf ) {
         }
     });
     scene_3D.add( mesh );
+    console.log(mesh.position);
 
     animations = gltf.animations;
     mixer = new AnimationMixer( mesh );  
@@ -95,17 +97,67 @@ loader.load( 'textures/cibus_ninja.glb', function ( gltf ) {
     // actions.forEach( function ( action ) {
     //     action.play();
     // } );
-    animate();
+
 });
 
-const cubeTexture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-const cubeGeometry = new THREE.BoxGeometry( 100, 100, 100 );
-const cubeMaterial = new THREE.MeshBasicMaterial( { map: cubeTexture } );
-cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-cube.scale.set(0.1,0.1,0.1);
-// cube.position.x = ;
-cube.position.z = -200;
-scene_3D.add( cube );
+// const cubeTexture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+// const cubeGeometry = new THREE.BoxGeometry( 100, 100, 100 );
+// const cubeMaterial = new THREE.MeshBasicMaterial( { map: cubeTexture } );
+// cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+// cube.scale.set(0.1,0.1,0.1);
+// cube.position.z = -20;
+// scene_3D.add( cube );
+
+// create a cube and add to scene
+const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+const cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff2255});
+cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.name = 'cube';
+scene_3D.add(cube);
+
+const cubeMaterial2 = new THREE.MeshLambertMaterial({color: 0xff0000});
+const cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial2);
+cube2.position.set(5, 0, 0);
+cube2.name = 'cube-red';
+scene_3D.add(cube2);
+
+
+const cubeMaterial3 = new THREE.MeshLambertMaterial({color: 0x00ff00});
+const cube3 = new THREE.Mesh(cubeGeometry, cubeMaterial3);
+cube3.position.set(0, 0, 5);
+cube3.name = 'cube-green';
+scene_3D.add(cube3);
+
+const cubeMaterial4 = new THREE.MeshLambertMaterial({color: 0x0000ff});
+const cube4 = new THREE.Mesh(cubeGeometry, cubeMaterial4);
+cube4.position.set(0, 0, -5);
+cube4.name = 'cube-blue';
+scene_3D.add(cube4);
+
+const cubeMaterial5 = new THREE.MeshLambertMaterial({color: 0xff00ff});
+const cube5 = new THREE.Mesh(cubeGeometry, cubeMaterial5);
+cube5.position.set(-5, 0, 0);
+cube5.name = 'cube-purple';
+scene_3D.add(cube5);
+
+cubes = [ cube2, cube3, cube4, cube5 ];
+
+let groundPlane = new THREE.PlaneGeometry(1000, 1000, 20, 20);
+let groundMat = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+    map: THREE.ImageUtils.loadTexture("../assets/textures/wood_1-1024x1024.png")
+});
+
+// groundMat.map.wrapS = groundMat.map?.wrapT = THREE.RepeatWrapping;
+// groundMat.map?.repeat.set(10, 10);
+
+
+var physMesh = new THREE.Mesh(groundPlane, groundMat);
+physMesh.rotation.x = -0.5 * Math.PI;
+physMesh.receiveShadow = true;
+physMesh.position.y = -2;
+
+scene_3D.add(physMesh);
 
 const controls = new OrbitControls(camera, renderer.domElement)
 camera.position.z = 1;
@@ -132,7 +184,6 @@ function press(keyCode: string) {
     if(keyCode === "ArrowLeft") {
         mesh.position.x += -0.1;
         mesh.position.y += 0;
-        console.log(hitTestRectangle(mesh, cube));
     } else if (keyCode === "ArrowUp") {
         mesh.position.x += 0;
         mesh.position.y += 0.1;
@@ -144,15 +195,26 @@ function press(keyCode: string) {
         mesh.position.y += -0.1;
     } else if (keyCode === " ") {
         attackAction.play();
+    } else if (keyCode === "Control") {
+        hitAction.play();
+    } else if (keyCode === "Alt") {
+        defendAction.play();
     } else {
         mesh.position.x += 0;
         mesh.position.y += 0;
     } 
+    console.log(mesh.position);
 }
 
 function release(keyCode: string) {
     if (keyCode === " ") {
         attackAction.stop();
+    } 
+    if (keyCode === "Control") {
+        hitAction.stop();
+    }
+    if (keyCode === "Alt") {
+        defendAction.stop();
     }
 }
 
@@ -172,40 +234,28 @@ function render() {
 
 animate();
 
-function hitTestRectangle(r1: THREE.Object3D, r2: THREE.Object3D) {
 
-    //Define the variables we'll need to calculate
-    let hit, combinedHalfWidths, combinedHalfHeights;
-    let vx: number, vy:number;
+function detectCollisionCubes(object1: THREE.Object3D, object2: THREE.Object3D) {
+    // object1.geometry.computeBoundingBox(); //not needed if its already calculated
+    // object2.geometry.computeBoundingBox();
+    object1.updateMatrixWorld();
+    object2.updateMatrixWorld();
+    
+    let box1 = object1.clone();
+    box1.applyMatrix4(object1.matrixWorld);
   
-    //hit will determine whether there's a collision
-    hit = false;
+    let box2 = object2.clone();
+    box2.applyMatrix4(object2.matrixWorld);
   
-    //Calculate the distance vector between the sprites
-    vx = (r1.position.x + r1.position.length() / 2) - 
-    (r2.position.x + r2.position.length() / 2);
-    // r1.centerX - r2.centerX;
-    vy = (r1.position.y + r1.position.length() / 2) - 
-    (r2.position.y + r2.position.length() / 2);
-    // r1.centerY - r2.centerY;
-  
-    //Figure out the combined half-widths and half-heights
-    combinedHalfWidths = r1.position.length() / 2 + r2.position.length() / 2;
-    // r1.halfWidth + r2.halfWidth;
-    combinedHalfHeights = r1.position.length() / 2 + r2.position.length() / 2;
-    // r1.halfHeight + r2.halfHeight;
-  
-    //Check for a collision on the x axis
-    console.log(r2.position);
-    console.log(r1.position);
-    if (r2.position < r1.position) {
-        //There's definitely a collision happening
-        hit = true;
-      } else {
-        //There's no collision on the y axis
-        hit = false;
-      }
-  
-    //`hit` will be either `true` or `false`
-    return hit;
-  };
+    return intersectsBox(box2);
+  }
+
+  function intersectsBox( box: THREE.Object3D ) {
+
+    // using 6 splitting planes to rule out intersections.
+    // return box. max.x < this.min.x || box.min.x > this.max.x ||
+    //     box.max.y < this.min.y || box.min.y > this.max.y ||
+    //     box.max.z < this.min.z || box.min.z > this.max.z ? false : true;
+
+}
+
